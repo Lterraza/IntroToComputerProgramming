3da31eed9287a5d4c2c5cb08be76bcfeec568559
Lterraza <74749687+Lterraza@users.noreply.github.com>
2021-01-21 22:09:06 -0700

    Add files via upload

---
+++ DateComparison/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DateComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime firstdate = new DateTime(1992, 11, 14);
+            DateTime seconddate = new DateTime(1992, 11, 14);
+            int result = DateTime.Compare(firstdate, seconddate);
+            switch (result)
+            {
+                case 0:
+                    Console.WriteLine("First Date {0} is equal to the Second Date {1}", firstdate.ToShortDateString(), seconddate.ToString());
+                    break;
+                case 1:
+                    Console.WriteLine("First Date {0} is greater than Second Date {1}", firstdate.ToString(), seconddate.ToString());
+                    break; 
+                case -1:
+                    Console.WriteLine("Second Date {0} is greater than First Date {1}", firstdate.ToString(), seconddate.ToString());
+                    break;
+
+            }
+
+        }
+    }
+}
+
