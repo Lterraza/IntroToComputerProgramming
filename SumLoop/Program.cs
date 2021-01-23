3c5c8caa64660e685e5d33109caadd620cdfbc71
Lterraza <74749687+Lterraza@users.noreply.github.com>
2021-01-22 17:30:18 -0700

    Add files via upload

---
+++ SumLoop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SumLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total=0;
+            for (int x = 1; x <=20 ;x++)
+            {
+                total += x;  
+            }
+            Console.WriteLine("Total = {0}", total);
+        }
+    }
+}
