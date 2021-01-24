9918f9af6ef82460f1b3ae102e23f45468c0afa4
Lterraza <74749687+Lterraza@users.noreply.github.com>
2021-01-23 22:45:38 -0700

    Add files via upload

---
+++ ArrayLists/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ArrayLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string allnames = string.Empty;
+            string[] namesArray = { "Luis ", "Adrian ", "Terrazas " };
+            foreach(string s in namesArray)
+
+            {
+                allnames += s;
+            }
+
+            Console.WriteLine(" using Array " + allnames);
+
+            string allintegers = string.Empty;
+            List<int> intList = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            foreach (int i in intList)
+            {
+
+                allintegers += i.ToString();
+            }
+            Console.WriteLine(" Using List " + allintegers);
+        }
+    }
+}
