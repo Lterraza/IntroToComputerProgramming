4ba3246d7ed3e0df0fc19b13d5007cbe1ddecd06
Lterraza <74749687+Lterraza@users.noreply.github.com>
2021-01-30 20:34:11 -0700

    Add files via upload

---
+++ Module8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Module8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User<int, string> u = new User<int, string>();
+            u.UserId = 1;
+            u.Name = "Adrian Terrazas";
+
+            Console.WriteLine(u.UserId.ToString() + " " + u.Name);
+        }
+    }
+}
---
+++ Module8/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Module8
+{
+    public class User<T, U>
+    {
+        public User(){}
+
+        public User(T userid, U name)
+        {
+            UserId = userid;
+            Name = name;
+        }
+
+        public T UserId { get; set; }
+
+        public U Name { get; set; }
+    }
+}
+
